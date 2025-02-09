6e6455c151fc3d4af824dcbeba7525048e2276ce
Xillyk <47789885+Xillyk@users.noreply.github.com>
2025-02-09 21:40:56 +0700

    add library cors to api-gateway, set allow origin for web fe

--- packages/api-gateway/index.ts
+++ packages/api-gateway/index.ts
@@ -1,12 +1,20 @@
 import express, { Application } from "express";
+import cors from "cors";
 
 const app: Application = express();
 const port = 3001;
 
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => res.send("LINE MAN Wongnai Frontend Assignment"));
+app.get("/", (req, res) =>
+  res.send({ data: "LINE MAN Wongnai Frontend Assignment" })
+);
 
 try {
   app.listen(port, (): void => {
