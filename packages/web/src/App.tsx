70deb6739ae982ed6ddb6f4dab2ef8ac88015795
Xillyk <47789885+Xillyk@users.noreply.github.com>
2025-02-10 21:30:09 +0700

    [web] implement infinite scroll loading menus

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,64 +1,113 @@
-import React, { useEffect, useState } from "react";
-import { getRestaurantInfo } from "./services/restaurantService";
-// import { IRestaurantInfo } from "../../types";
-import { isRestaurantOpen } from "./utils/timeCheck";
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { getRestaurantInfoAndMenus } from "./services/restaurantService";
 import { useRestaurant } from "./contexts/RestaurantContext";
+import { IShortMenu } from "../../types";
+import { isRestaurantOpen } from "./utils/timeCheck";
 
 const restaurantIds = ["567051", "227018"];
 
 const App = () => {
   const { restaurantInfo, setRestaurantInfo } = useRestaurant();
-  console.log("🚀 ~ App ~ restaurantInfo:", restaurantInfo);
+  const [menus, setMenus] = useState<IShortMenu[]>([]);
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+  const observerBottomRef = useRef<HTMLDivElement | null>(null);
+
+  const fetchData = useCallback(async () => {
+    if (!hasMore || isLoading) return;
+
+    setIsLoading(true);
+    const data = await getRestaurantInfoAndMenus(restaurantIds[1], page, 10);
+    console.log("🚀 ~ fetchData ~ data:", data);
+
+    if (data) {
+      setRestaurantInfo(data.info);
+      setMenus((prev) => [...prev, ...data.menus]);
+      setIsOpen(isRestaurantOpen(data.info.activeTimePeriod));
+      setHasMore(data.hasMore);
+      setPage((prev) => prev + 1);
+    }
+    setIsLoading(false);
+  }, [page, hasMore, isLoading, setRestaurantInfo]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await getRestaurantInfo(restaurantIds[0]);
-
-      if (data) {
-        setRestaurantInfo(data);
-        setIsOpen(isRestaurantOpen(data.activeTimePeriod));
-      }
-    };
-
     fetchData();
   }, []);
 
-  // const fetchMenus = async () => {
-  //   const numToFetch = 10;
-  //   const menusList = restaurantInfo?.menus.slice(0, numToFetch);
+  useEffect(() => {
+    if (!observerBottomRef.current) return;
 
-  //   // call api
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          fetchData();
+        }
+      },
+      { threshold: 1.0 }
+    );
 
-  //   // backend serve first 10 for the first time (with flag)
-  // };
+    if (observerBottomRef.current) observer.observe(observerBottomRef.current);
+
+    return () => observer.disconnect();
+  }, [fetchData, hasMore]);
 
   return (
     <div className="h-screen bg-red-200 overflow-auto">
-      {restaurantInfo && (
+      {restaurantInfo ? (
         <>
           {/* cover image */}
+          <img src={restaurantInfo.coverImage} alt={restaurantInfo.name} />
 
-          <img src={restaurantInfo.coverImage} alt="restaurantInfo image" />
+          <div className="flex items-center gap-2">
+            {/* name */}
+            <h1 className="text-4xl font-bold">{restaurantInfo.name}</h1>
 
-          {/* name */}
-          <h1 className="text-4xl font-bold">{restaurantInfo.name}</h1>
-
-          <div
-            className={`${
-              isOpen ? "bg-green-500" : "bg-red-500"
-            } w-fit px-6 py-1 rounded-xl text-white`}
-          >
-            <span>{isOpen ? "Open" : "Close"}</span>
+            {/* status */}
+            <div
+              className={`${
+                isOpen ? "bg-green-500" : "bg-red-500"
+              } w-fit px-6 py-1 rounded-xl text-white`}
+            >
+              <span>{isOpen ? "เปิด" : "ปิด"}</span>
+            </div>
           </div>
 
-          {/* status */}
-
           {/* menu list */}
-          {restaurantInfo.menus.map((menu, index) => (
-            <div key={index}>{menu}</div>
-          ))}
+          <div className="mt-4">
+            {menus.map((menu, index) => (
+              <div
+                key={index}
+                className="h-[140px] rounded-2xl w-full flex flex-col gap-4 cursor-pointer"
+              >
+                <div className="flex gap-4">
+                  <div>
+                    <img
+                      src={menu.thumbnailImage}
+                      alt={menu.name}
+                      className="size-[100px] rounded-2xl"
+                    />
+                  </div>
+
+                  <div className="flex flex-col">
+                    <span>{menu.name}</span>
+                    <span>{`${menu.fullPrice} บาท`}</span>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div
+            ref={observerBottomRef}
+            style={{ height: "20px", background: "transparent" }}
+          />
+          {isLoading && <p>loading more</p>}
         </>
+      ) : (
+        <>No data</>
       )}
     </div>
   );
--- packages/web/src/services/restaurantService.ts
+++ packages/web/src/services/restaurantService.ts
@@ -1,14 +1,18 @@
-import { IRestaurantInfo } from "../../../types";
+import { IRestaurantInfoAndMenusPagination } from "../../../types";
 
 const API_GATEWAY_ENDPOINT = import.meta.env.VITE_API_GATEWAY_ENDPOINT;
 // get restaurant info
-export const getRestaurantInfo = async (id: string) => {
+export const getRestaurantInfoAndMenus = async (
+  restaurantId: string,
+  page: number,
+  limit: number
+) => {
   try {
     const response = await fetch(
-      `${API_GATEWAY_ENDPOINT}/restaurant/info/${id}/10`
+      `${API_GATEWAY_ENDPOINT}/restaurant/info/${restaurantId}?page=${page}&limit=${limit}`
     );
-    const data = await response.json();
-    return data.data as IRestaurantInfo;
+    const data: IRestaurantInfoAndMenusPagination = await response.json();
+    return data;
   } catch (error) {
     console.log("🚀 ~ getRestaurantInfo ~ error:", error);
   }
