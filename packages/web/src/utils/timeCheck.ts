13e5dff3f5f1b254843c4782bee163cfdaf5f1cc
Xillyk <47789885+Xillyk@users.noreply.github.com>
2025-02-10 21:29:23 +0700

    [web] add util to check that is restaurant is currently close or open

---
+++ packages/web/src/utils/timeCheck.ts
@@ -0,0 +1,28 @@
+import { IRestaurantInfo } from "../../../types";
+
+// is in open time range
+export const isRestaurantOpen = (
+  activeTimePeriod: IRestaurantInfo["activeTimePeriod"]
+) => {
+  const now = new Date();
+  const currentHours = now.getHours();
+  const currentMinutes = now.getMinutes();
+
+  // Convert current time to total minutes
+  const currentTimeInMinutes = currentHours * 60 + currentMinutes;
+
+  // Convert open and close times to total minutes
+  const [openHours, openMinutes] = activeTimePeriod.open.split(":").map(Number);
+  const openTimeInMinutes = openHours * 60 + openMinutes;
+
+  const [closeHours, closeMinutes] = activeTimePeriod.close
+    .split(":")
+    .map(Number);
+  const closeTimeInMinutes = closeHours * 60 + closeMinutes;
+
+  // Check if the current time is within the range
+  return (
+    currentTimeInMinutes >= openTimeInMinutes &&
+    currentTimeInMinutes <= closeTimeInMinutes
+  );
+};
