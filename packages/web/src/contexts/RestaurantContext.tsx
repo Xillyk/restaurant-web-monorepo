374f656e5568513f9dad84ba0a52ba28544bea2f
Xillyk <47789885+Xillyk@users.noreply.github.com>
2025-02-11 13:15:35 +0700

    [web] update restaurant context

--- packages/web/src/contexts/RestaurantContext.tsx
+++ packages/web/src/contexts/RestaurantContext.tsx
@@ -1,25 +1,50 @@
 import React, { createContext, useState, ReactNode, useContext } from "react";
-import { IRestaurantInfo } from "../../../types";
+import { IRestaurantInfo, IShortMenu } from "../../../types";
 
 interface IRestaurantContext {
   restaurantInfo: IRestaurantInfo | undefined;
   setRestaurantInfo: React.Dispatch<
     React.SetStateAction<IRestaurantInfo | undefined>
   >;
+  selectedRestaurantId: IRestaurantInfo["id"] | undefined;
+  setSelectedRestaurantId: React.Dispatch<
+    React.SetStateAction<IRestaurantInfo["id"] | undefined>
+  >;
+  selectedMenuId: IShortMenu["id"] | undefined;
+  setSelectedMenuId: React.Dispatch<
+    React.SetStateAction<IShortMenu["id"] | undefined>
+  >;
 }
 
 const RestaurantContext = createContext<IRestaurantContext>({
   restaurantInfo: undefined,
   setRestaurantInfo: () => {},
+  selectedRestaurantId: undefined,
+  setSelectedRestaurantId: () => {},
+  selectedMenuId: undefined,
+  setSelectedMenuId: () => {},
 });
 
 export const RestaurantProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
   const [restaurantInfo, setRestaurantInfo] = useState<IRestaurantInfo>();
+  const [selectedRestaurantId, setSelectedRestaurantId] =
+    useState<IRestaurantInfo["id"]>();
+
+  const [selectedMenuId, setSelectedMenuId] = useState<IShortMenu["id"]>();
 
   return (
-    <RestaurantContext.Provider value={{ restaurantInfo, setRestaurantInfo }}>
+    <RestaurantContext.Provider
+      value={{
+        restaurantInfo,
+        setRestaurantInfo,
+        selectedRestaurantId,
+        setSelectedRestaurantId,
+        selectedMenuId,
+        setSelectedMenuId,
+      }}
+    >
       {children}
     </RestaurantContext.Provider>
   );
