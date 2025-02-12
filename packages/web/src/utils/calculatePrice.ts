851316b89813dec78e634b0c6cab697f95dfb71e
Xillyk <47789885+Xillyk@users.noreply.github.com>
2025-02-12 18:36:40 +0700

    [web] add get menu price util

---
+++ packages/web/src/utils/calculatePrice.ts
@@ -0,0 +1,16 @@
+import { checkIsInDiscountPeriod } from "./timeCheck";
+
+import { IFullMenu, IShortMenu } from "../../../types";
+
+export const getMenuPrice = (menu: IShortMenu | IFullMenu) => {
+  if (
+    menu.discountedPercent > 0 &&
+    checkIsInDiscountPeriod(menu.discountedTimePeriod)
+  ) {
+    return Math.round(
+      menu.fullPrice - (menu.fullPrice * menu.discountedPercent) / 100
+    );
+  }
+  // no discount || not in discount period will return full price
+  return menu.fullPrice;
+};
