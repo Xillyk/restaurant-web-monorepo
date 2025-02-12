be3a6de57fc57cd24cd779febce110b8fbb5e336
Xillyk <47789885+Xillyk@users.noreply.github.com>
2025-02-12 18:35:19 +0700

    [api] mock discount, discountPeriod for all menus

--- packages/api-gateway/src/routes/restaurant.ts
+++ packages/api-gateway/src/routes/restaurant.ts
@@ -35,10 +35,25 @@ router.get("/info/:id", async (req: Request, res: Response) => {
 
       const data = await fetchAllMenus(restaurantId, slicedMenus, limit);
 
+      const allMenuLength = restaurantInfo.menus.length;
+
+      // no need to send now
+      restaurantInfo.menus = [];
+
+      // ?: set discount, and period
+      const editedMenuData = data.map((menu) => ({
+        ...menu,
+        discountedPercent: 10,
+        discountedTimePeriod: {
+          begin: "16:00",
+          end: "19:00",
+        },
+      }));
+
       const responseData: IRestaurantInfoAndMenusPagination = {
         info: restaurantInfo,
-        menus: data,
-        hasMore: endIndex < restaurantInfo.menus.length,
+        menus: editedMenuData,
+        hasMore: endIndex < allMenuLength,
       };
 
       res.status(200).send({
@@ -64,8 +79,18 @@ router.get("/full-menu", async (req: Request, res: Response) => {
       );
       const menuData = (await menuResponse.json()) as IFullMenu;
 
-      res.status(200).send({
+      // ?: set discount, and period
+      const editedMenuData = {
         ...menuData,
+        discountedPercent: 10,
+        discountedTimePeriod: {
+          begin: "16:00",
+          end: "19:00",
+        },
+      };
+
+      res.status(200).send({
+        ...editedMenuData,
       });
     } catch (error) {
       res.status(500).send({ message: `target api response with error` });
@@ -91,12 +116,25 @@ router.get("/top-menu", async (req: Request, res: Response) => {
       // * 2 fetch all menus
       const menus = await fetchAllMenus(restaurantId, restaurantInfo.menus, 20);
 
+      // menus.forEach((menu) => console.log(menu.discountedTimePeriod));
+      // console.log("🚀 ~ router.get ~ t:", t);
+
       // * sort top sold menus
       const filterPopularMenus = menus
         .sort((a, b) => b.sold - a.sold)
         .slice(0, limit);
 
-      res.status(200).send(filterPopularMenus);
+      // ?: set discount, and period
+      const editedMenuData = filterPopularMenus.map((menu) => ({
+        ...menu,
+        discountedPercent: 10,
+        discountedTimePeriod: {
+          begin: "16:00",
+          end: "19:00",
+        },
+      }));
+
+      res.status(200).send(editedMenuData);
     } catch (error) {
       res.status(500).send({ message: `target api response with error` });
     }
