9107c9d84507ba8a3dbaa15e1e323ddfaa23409b
Xillyk <47789885+Xillyk@users.noreply.github.com>
2025-02-14 07:04:29 +0700

    [api] split express app declaration and app listening

--- packages/api-gateway/index.ts
+++ packages/api-gateway/index.ts
@@ -1,27 +1,6 @@
-import express, { Application } from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import app from "./server.ts";
 
-import restaurantRoute from "./src/routes/restaurant.ts";
-
-const app: Application = express();
 const port = process.env.API_PORT || 3002;
-
-dotenv.config();
-
-app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-  })
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/restaurant", restaurantRoute);
-
-app.get("/", (req, res) =>
-  res.send({ data: "LINE MAN Wongnai Frontend Assignment" })
-);
 
 try {
   app.listen(port, (): void => {
---
+++ packages/api-gateway/server.ts
@@ -0,0 +1,25 @@
+import express, { Application } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import restaurantRoute from "./src/routes/restaurant.ts";
+
+const app: Application = express();
+
+dotenv.config();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) =>
+  res.send({ data: "LINE MAN Wongnai Frontend Assignment" })
+);
+
+app.use("/restaurant", restaurantRoute);
+
+export default app;
