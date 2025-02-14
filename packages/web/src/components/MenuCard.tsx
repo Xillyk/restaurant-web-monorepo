a4e1d1a9fb329349fcca817e3c4cc42c8006c72a
Xillyk <47789885+Xillyk@users.noreply.github.com>
2025-02-14 08:33:16 +0700

    [web] fix menu card text responsive

--- packages/web/src/components/MenuCard.tsx
+++ packages/web/src/components/MenuCard.tsx
@@ -33,7 +33,7 @@ const MenuCard = ({ menu, isRecommendList }: MenuCardProps) => {
 
   return (
     <div
-      className="h-[100px] md:h-[120px] 2xl:h-[140px] rounded-2xl w-2xs sm:w-xs md:w-lg lg:w-xl flex gap-4 cursor-pointer relative"
+      className="h-[100px] md:h-[120px] 2xl:h-[140px] rounded-2xl w-xs sm:w-xs md:w-lg lg:w-xl flex gap-4 cursor-pointer relative"
       onClick={() => handleClickMenu()}
     >
       {/* top sell badge */}
@@ -49,40 +49,43 @@ const MenuCard = ({ menu, isRecommendList }: MenuCardProps) => {
       />
 
       <div className="flex flex-col gap-1">
-        <span className="text-md md:text-xl font-semibold line-clamp-2">
+        <span className="text-sm md:text-xl font-semibold line-clamp-2">
           {menu.name}
         </span>
-        <div className="flex items-center gap-2">
-          {menu.discountedPercent > 0 &&
-          checkIsInDiscountPeriod(menu.discountedTimePeriod) ? (
-            <div className="text-lg md:text-xl font-medium flex gap-2 items-center">
-              <span
-                className={`${"line-through decoration-2 text-red-500"} text-xs md:text-lg`}
-              >
-                {menu.fullPrice}
+
+        <div className="flex sm:block items-center gap-2">
+          <div className="flex items-center gap-2">
+            {menu.discountedPercent > 0 &&
+            checkIsInDiscountPeriod(menu.discountedTimePeriod) ? (
+              <div className="text-lg md:text-xl font-medium flex gap-2 items-center">
+                <span
+                  className={`${"line-through decoration-[2px] text-red-500"} text-sm md:text-lg`}
+                >
+                  {menu.fullPrice}
+                </span>
+
+                <span className="text-green-600">{getMenuPrice(menu)}</span>
+              </div>
+            ) : (
+              // no discount -> show full price
+              <span className={`text-sm md:text-xl font-medium text-gray-700`}>
+                {getMenuPrice(menu)}
               </span>
+            )}
 
-              <span className="text-green-600">{getMenuPrice(menu)}</span>
-            </div>
-          ) : (
-            // no discount -> show full price
-            <span className={`text-sm md:text-xl font-medium text-gray-700`}>
-              {getMenuPrice(menu)}
+            <span className="text-sm md:text-lg font-normal text-gray-600">
+              บาท
             </span>
-          )}
+          </div>
 
-          <span className="text-sm md:text-lg font-normal text-gray-600">
-            บาท
-          </span>
+          {/* discount badge */}
+          {menu.discountedPercent > 0 &&
+            checkIsInDiscountPeriod(menu.discountedTimePeriod) && (
+              <div className="flex items-center gap-1 bg-orange-500 text-white w-fit px-2 md:px-4 rounded-xl py-[1px] text-sm md:text-lg md:mt-1">
+                {menu.discountedPercent} <RiDiscountPercentLine size={20} />
+              </div>
+            )}
         </div>
-
-        {/* discount badge */}
-        {menu.discountedPercent > 0 &&
-          checkIsInDiscountPeriod(menu.discountedTimePeriod) && (
-            <div className="flex items-center gap-1 bg-orange-500 text-white w-fit px-3 rounded-xl py-[1px] text-md">
-              {menu.discountedPercent} <RiDiscountPercentLine size={20} />
-            </div>
-          )}
       </div>
     </div>
   );
